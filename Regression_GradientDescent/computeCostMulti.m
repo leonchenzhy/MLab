3dd6f3cf8f2cb5fcf8d85756d7aecc6a9e3d2007
Leon Chen <neo.chenzhy@gmail.com>
2015-12-12 00:42:12 -0800

    Update computeCostMulti.m

--- Regression_GradientDescent/computeCostMulti.m
+++ Regression_GradientDescent/computeCostMulti.m
@@ -9,15 +9,8 @@ m = length(y); % number of training examples
 % You need to return the following variables correctly 
 J = 0;
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta
-%               You should set J to the cost.
-
 predictions = X*theta;
 E = (predictions - y);
 J = (E'* E) * 1/(2*m);
-
-
-% =========================================================================
 
 end
