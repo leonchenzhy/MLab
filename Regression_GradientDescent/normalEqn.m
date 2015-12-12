72d4c11b51ba9fff61aca404585c38f4635aa31c
Leon Chen <neo.chenzhy@gmail.com>
2015-12-12 00:44:20 -0800

    Update normalEqn.m

--- Regression_GradientDescent/normalEqn.m
+++ Regression_GradientDescent/normalEqn.m
@@ -4,20 +4,6 @@ function [theta] = normalEqn(X, y)
 %   regression using the normal equations.
 
 theta = zeros(size(X, 2), 1);
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Complete the code to compute the closed form solution
-%               to linear regression and put the result in theta.
-%
-
-% ---------------------- Sample Solution ----------------------
-
 theta = pinv(X'* X) * X' * y;
-
-
-% -------------------------------------------------------------
-
-
-% ============================================================
 
 end
