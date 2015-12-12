16ba0003dddf391bf4ece3bca2ad542afce623b9
Leon Chen <neo.chenzhy@gmail.com>
2015-12-12 00:43:36 -0800

    Update featureNormalize.m

--- Regression_GradientDescent/featureNormalize.m
+++ Regression_GradientDescent/featureNormalize.m
@@ -10,34 +10,10 @@ X_norm = X;
 mu = zeros(1, size(X, 2));
 sigma = zeros(1, size(X, 2));
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: First, for each feature dimension, compute the mean
-%               of the feature and subtract it from the dataset,
-%               storing the mean value in mu. Next, compute the 
-%               standard deviation of each feature and divide
-%               each feature by it's standard deviation, storing
-%               the standard deviation in sigma. 
-%
-%               Note that X is a matrix where each column is a 
-%               feature and each row is an example. You need 
-%               to perform the normalization separately for 
-%               each feature. 
-%
-% Hint: You might find the 'mean' and 'std' functions useful.
-%       
 for i = 1:size(X,2)
     mu(i) = mean(X(:,i));
     sigma(i) = std(X(:,i));
     X_norm(:,i) = (X(:,i) - mu(:,i))/sigma(:,i);
 end    
-
-
-
-
-
-
-
-
-% ============================================================
 
 end
