584167ecc321ecbfe896f1e0909897ed9dcb8602
Leon Chen <ZYCHEN@Neo-MacBook-Pro.hsd1.wa.comcast.net>
2016-01-30 00:01:52 -0800

    Recommendation & Anomaly Detection

---
+++ Recommendation & Anomaly Detection/estimateGaussian.m
@@ -0,0 +1,20 @@
+function [mu sigma2] = estimateGaussian(X)
+%ESTIMATEGAUSSIAN This function estimates the parameters of a 
+%Gaussian distribution using the data in X
+%   [mu sigma2] = estimateGaussian(X), 
+%   The input X is the dataset with each n-dimensional data point in one row
+%   The output is an n-dimensional vector mu, the mean of the data set
+%   and the variances sigma^2, an n x 1 vector
+% 
+
+% Useful variables
+[m, n] = size(X);
+
+mu = zeros(n, 1);
+sigma2 = zeros(n, 1);
+
+
+mu = mean(X);
+sigma2 = ((m-1)/m) * std(X).^2;
+
+end
